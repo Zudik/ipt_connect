7bf39dc3132120451f4915f1bf6457e85c17d495
Zudik <Vutden1@yandex.ru>
2019-05-15 22:51:00 +0300

    Functions for replace the default templates

--- ipt_connect/ipt_connect/views.py
+++ ipt_connect/ipt_connect/views.py
@@ -1,4 +1,6 @@
 from django.http import HttpResponse
+from django.shortcuts import render_to_response
+from django.template import RequestContext
 
 def home(request):
 
@@ -6,3 +8,9 @@ def home(request):
 
               <a href="http://connect.iptnet.info/IPTdev">Results of IPT dev</a>"""
     return HttpResponse(text)
+
+def custom_404(request):
+    return render_to_response('404.html', RequestContext(request))
+
+def custom_500(request):
+    return render_to_response('500.html', RequestContext(request))
